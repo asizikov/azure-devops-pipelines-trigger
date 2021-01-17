382d66f69c71a3aed28669f1328a3c8f524dfd77
Anton Sizikov <sizikov.anton@gmail.com>
2021-01-17 21:58:16 +0100

    trigger build on webhook

--- src/CloudEng.Pipelines.Trigger/Api/BranchDeletedWebhook.cs
+++ src/CloudEng.Pipelines.Trigger/Api/BranchDeletedWebhook.cs
@@ -1,24 +1,64 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using CloudEng.Pipelines.Trigger.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
 using Newtonsoft.Json;
 
 namespace CloudEng.Pipelines.Trigger.Api
 {
-    public static class BranchDeletedWebhook
+    public class BranchDeletedWebhook
     {
-        [FunctionName("BranchDeletedWebhook")]
-        public static async Task<IActionResult> RunAsync(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
-            HttpRequest req, ILogger log)
+        private const string collectionUri = "https://dev.azure.com/asizikov";
+        private const string projectName = "pulumi-azure";
+        private string pat = "";
+        private const string pipelineName = "cleanup-resources";
+
+        public BranchDeletedWebhook()
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            pat = Environment.GetEnvironmentVariable("ACCESS_TOKEN");
+        }
+        
+        [FunctionName("BranchDeletedWebhook")]
+        public async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
+            GitHubEvent gitHubEvent, ILogger log)
+        {
+            if (gitHubEvent is null || !gitHubEvent.Ref.StartsWith("feature/") || gitHubEvent.Ref_Type != "branch")
+            {
+                log.LogInformation("unknown event payload, going to skip");
+                return new OkResult();
+            }
+            
+            var creds = new VssBasicCredential(string.Empty, pat);
+            var connection = new VssConnection(new Uri(collectionUri), creds);
+            var buildHttpClient = connection.GetClient<BuildHttpClient>();
+            var buildDefinitionReferences = await buildHttpClient.GetDefinitionsAsync(projectName);
+
+            var buildDefinitionReference = buildDefinitionReferences.FirstOrDefault(reference => reference.Name == pipelineName);
+            if (buildDefinitionReference is null)
+            {
+                log.LogWarning($"Expected to find {pipelineName} build definition reference");
+                return new OkResult();
+            }
+            
+            var branchName = gitHubEvent.Ref.Replace('/', '-');
+            var build = new Build {
+                Definition = buildDefinitionReference,
+                Parameters = $"{{\"branchName\":\"{branchName}\"}}"
+            };
+            
+            var buildQueueResult = await buildHttpClient.QueueBuildAsync(build, projectName);
+            log.LogInformation(buildQueueResult.Status.ToString());
             return new OkResult();
         }
     }
---
+++ src/CloudEng.Pipelines.Trigger/Models/GitHubEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace CloudEng.Pipelines.Trigger.Models
+{
+    public class GitHubEvent
+    {
+        public string Ref { get; set; }
+        public string Ref_Type { get; set; }
+        public Repository Repository { get; set; }
+    }
+
+    public class Repository
+    {
+        public string Name { get; set; }
+    }
+}
